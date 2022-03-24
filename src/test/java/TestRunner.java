55b36e71315bcae3bc0711426ffcea7317bdb2b1
Aleksandr Korostelev <akorostelev@fil-it.ru>
2022-03-24 18:30:44 +0300

    Add example for reports

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,9 @@
+import cucumber.api.*;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import net.thucydides.core.annotations.Managed;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+
+public class TestRunner {
+}
